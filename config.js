6863f8b8223ae6902a84de26ab5aae7377ab56f3
JustynaAnna <justynkapawlik@gmail.com>
2023-10-14 20:47:06 +0200

    remoe config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-const config = {
-    apiKey: "93c17107b9d0b9d75a192e66ddb20e38",
-    apiUrl: "https://api.openweathermap.org/data/2.5/forecast?units=metric&q=",
-  };
-  
-  export default config;
